c3f6602263f658b7edd6ddc72083fd8622aba7cc
ghospadi <Lolr2223@gmail.com>
2022-06-09 20:43:09 +0300

    finished work with libraries

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let instance;
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function galleryCreator(pics) {
+  return pics
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+    alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join("");
+}
+
+const gallery = galleryCreator(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
+
+galleryContainer.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`);
+  instance.show();
+  closeModal();
+}
+
+function closeModal() {
+  document.addEventListener("keydown", (e) => {
+    if (e.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function galleryCreator(pics) {
+  return pics
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join("");
+}
+
+const gallery = galleryCreator(galleryItems);
+galleryContainer.insertAdjacentHTML("afterbegin", gallery);
+
+galleryContainer.addEventListener("click", (e) => e.preventDefault());
+
+var lightbox = new SimpleLightbox(".gallery a");
+lightbox.options.captionsData = "alt";
+lightbox.options.captionType = "attr";
+lightbox.options.captionDelay = 250;
